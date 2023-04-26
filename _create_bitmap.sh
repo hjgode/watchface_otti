fe6f4445aa5c0a21b33871fc331e453acdc5cb27
hgode <hjgode@gmail.com>
2023-04-26 06:41:08 +0200

    added some glyph names for later use

--- _create_bitmap.sh
+++ _create_bitmap.sh
@@ -6,7 +6,23 @@ if [ -z "$1" ]
     exit 1
 fi
 
+name=$1
 
-fontimage --pixelsize 22 --text "$1" -o "$1_comicsans1.png" ComicSansMSBold.ttf
-convert "$1_comicsans1.png" -negate "$1_comicsans_n.png"
-convert "$1_comicsans_n.png" -transparent black "$1_comicsans.png"
+if [ $name eq "." ]
+  then
+    $name="period"
+elif [ $name eq ":" ]
+  then
+    $name="colon"
+elif [ $name eq "!" ]
+  then
+    $name="exclamation"
+elif [ $name eq "%" ]
+  then
+    $name="percent"
+fi
+
+echo "output to: ${name}_comicsans.png"
+fontimage --pixelsize 22 --text "${1}" -o ${name}_comicsans1.png ComicSansMSBold.ttf
+convert ${name}_comicsans1.png -negate ${name}_comicsans_n.png
+convert ${name}_comicsans_n.png -transparent black ${name}_comicsans.png
